1b959bcd25f93ffaa60f14249d3e9cdf0f7f935c
Edson <costaedson49@gmail.com>
2026-06-29 09:29:13 -0300

    feat: tarefa colega 3

--- src/model/pagamentos/PagamentoCartao.java
+++ src/model/pagamentos/PagamentoCartao.java
@@ -1,4 +1,15 @@
 package model.pagamentos;
 
-public class PagamentoCartao {
+public class PagamentoCartao extends Pagamento {
+    private String tipo;
+
+    public PagamentoCartao(double valor, String tipo) {
+        super(valor);
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void processarPagamento() {
+        System.out.println("Pagamento processado: Tipo = " + this.tipo + ", Valor = " + getValor());
+    }
 }
--- src/model/pagamentos/PagamentoDinheiro.java
+++ src/model/pagamentos/PagamentoDinheiro.java
@@ -1,4 +1,20 @@
 package model.pagamentos;
 
-public class PagamentoDinheiro {
+public class PagamentoDinheiro extends Pagamento {
+    private double valorRecebido;
+
+    public PagamentoDinheiro(double valor, double valorRecebido) {
+        super(valor);
+        this.valorRecebido = valorRecebido;
+    }
+
+    @Override
+    public void processarPagamento() {
+        if (valorRecebido < getValor()) {
+            throw new RuntimeException("Valor recebido é menor que o valor do pagamento.");
+        } else {
+            double troco = valorRecebido - getValor();
+            System.out.println("Pagamento em dinheiro processado. Troco: " + troco);
+        }
+    }
 }
